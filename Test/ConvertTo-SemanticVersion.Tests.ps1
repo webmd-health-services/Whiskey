12ba7d20d97b3b18b6c5b09a027de18e4eeed19e
Aaron Jensen <ajensen@webmd.net>
2017-01-03 16:42:43 -0800

    ConvertTo-SemanticVersion improvements:
    * Made it public.
    * Added support for parsing numeric values, e.g. doubles and integers.
    * Added support for automatically adding build metadata.

--- Test/ConvertTo-SemanticVersion.Tests.ps1
+++ Test/ConvertTo-SemanticVersion.Tests.ps1
@@ -12,14 +12,56 @@ $testCases = @{
                 2.001 = '2.1.0';
                 3 = '3.0.0';
                 '5.6.7-rc.3' = '5.6.7-rc.3';
+                '3.2.1+build.info' = '3.2.1';
               }
 
 foreach( $key in $testCases.Keys )
 {
-    Describe ('ConvertTo-SemanticVersion.when converting ''[{0}]{1}''' -f $key.GetType().Name.ToLowerInvariant(),$key) {
-        $expectedValue = [SemVersion.SemanticVersion]$testCases[$key]
-        It ('should be ''{0}''' -f $expectedValue) {
-            $key | ConvertTo-SemanticVersion | Should Be $expectedValue
+    $semanticVersion = [SemVersion.SemanticVersion]$testCases[$key]
+    $withBuildMetadata = $semanticVersion
+    if( $key -match '\+' )
+    {
+        $withBuildMetadata = [SemVersion.SemanticVersion]$key
+    }
+
+    Describe ('ConvertTo-SemanticVersion.when converting ''[{0}]{1}'' by a developer' -f $key.GetType().Name.ToLowerInvariant(),$key) {
+        Mock -CommandName 'Test-WhsCIRunByBuildServer' -ModuleName 'WhsCI' -MockWith { return $false }
+        $buildInfo = '{0}@{1}' -f $env:USERNAME,$env:COMPUTERNAME
+        $expectedSemVer = New-Object 'SemVersion.SemanticVersion' $semanticVersion.Major,$semanticVersion.Minor,$semanticVersion.Patch,$semanticVersion.Prerelease,$buildInfo
+        It ('should be ''{0}''' -f $expectedSemVer) {
+            $key | ConvertTo-SemanticVersion | Should Be $expectedSemVer
+        }
+
+        It 'should include build information' {
+            $key | ConvertTo-SemanticVersion | ForEach-Object { $_.ToString() } | Should Be $expectedSemVer.ToString()
+        }
+
+        It 'should preserve build information' {
+            $key | ConvertTo-SemanticVersion -PreserveBuildMetadata | ForEach-Object { $_.ToString() } | Should Be $withBuildMetadata.ToString()
+        }
+    }
+
+    Describe ('ConvertTo-SemanticVersion.when converting ''[{0}]{1}'' by build server' -f $key.GetType().Name.ToLowerInvariant(),$key) {
+        Mock -CommandName 'Test-WhsCIRunByBuildServer' -ModuleName 'WhsCI' -MockWith { return $true }
+        $buildID = '80'
+        $branch = 'origin/develop'
+        $commitID = 'deadbeefdeadbeefdeadbeefdeadbeef'
+        Mock -CommandName 'Get-Item' -ModuleName 'WhsCI' -MockWith { [pscustomobject]@{ Value = $buildID } }.GetNewClosure() -ParameterFilter { $Path -eq 'env:BUILD_ID' }
+        Mock -CommandName 'Get-Item' -ModuleName 'WhsCI' -MockWith { [pscustomobject]@{ Value = $branch } }.GetNewClosure() -ParameterFilter { $Path -eq 'env:GIT_BRANCH' }
+        Mock -CommandName 'Get-Item' -ModuleName 'WhsCI' -MockWith { [pscustomobject]@{ Value = $commitID } }.GetNewClosure() -ParameterFilter { $Path -eq 'env:GIT_COMMIT' }
+        
+        $buildInfo = '{0}.develop.{2}' -f $buildID,'develop',$commitID.Substring(0,7)
+        $expectedSemVer = New-Object 'SemVersion.SemanticVersion' $semanticVersion.Major,$semanticVersion.Minor,$semanticVersion.Patch,$semanticVersion.Prerelease,$buildInfo
+        It ('should be ''{0}''' -f $expectedSemVer) {
+            $key | ConvertTo-SemanticVersion | Should Be $expectedSemVer
+        }
+
+        It 'should include build information' {
+            $key | ConvertTo-SemanticVersion | ForEach-Object { $_.ToString() } | Should Be $expectedSemVer.ToString()
+        }
+
+        It 'should preserve build information' {
+            $key | ConvertTo-SemanticVersion -PreserveBuildMetadata | ForEach-Object { $_.ToString() } | Should Be $withBuildMetadata.ToString()
         }
     }
 }
