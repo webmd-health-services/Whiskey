84f9fb4734a1e8f03b7635d358baf311fba2061f
Jeffrey Price <jtprice@webmd.net>
2017-04-17 16:19:12 -0700

    Removed ProGetAutomation from being imported as a public module

--- WhsCI/Import-WhsCI.ps1
+++ WhsCI/Import-WhsCI.ps1
@@ -5,11 +5,3 @@ if( (Get-Module -Name 'WhsCI') )
 }
 
 Import-Module (Join-Path -Path $PSScriptRoot -ChildPath 'WhsCI.psd1' -Resolve)
-
-
-if( (Get-Module -Name 'ProGetAutomation') )
-{
-    Remove-Module -Name 'ProGetAutomation' -Force
-}
-
-Import-Module (Join-Path -Path $PSScriptRoot -ChildPath '.\ProGetAutomation\ProGetAutomation.psm1' -Resolve)
