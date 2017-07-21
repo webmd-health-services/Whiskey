d80d0bd798db295193d094b7b1b7a54e4765945c
Aaron Jensen <ajensen@webmd.net>
2017-07-21 11:26:08 -0700

    Adding properties for handling clean/initialization modes.

--- Whiskey/Whiskey.psm1
+++ Whiskey/Whiskey.psm1
@@ -1,7 +1,11 @@
 
 $events = @{ }
 
-Add-Type -TypeDefinition @"
+$type = [AppDomain]::CurrentDomain.GetAssemblies() | ForEach-Object { $_.GetType('Whiskey.TaskAttribute') }
+
+if( -not $type )
+{
+    Add-Type -TypeDefinition @"
 
 namespace Whiskey {
 
@@ -13,10 +17,23 @@ namespace Whiskey {
         }
 
         public string Name { get; private set; }
+
+        public bool SupportsClean { get; set; }
+
+        public bool SupportsInitialize { get; set; }
     }
+
 }
 
-"@
+"@ -ErrorAction Ignore
+}
+
+$attr = New-Object -TypeName 'Whiskey.TaskAttribute' -ArgumentList 'Whiskey' -ErrorAction Ignore
+if( -not ($attr | Get-Member 'SupportsClean') )
+{
+    Write-Error -Message ('You''ve got an old version of Whiskey loaded. Please open a new PowerShell session.') -ErrorAction Stop
+}
+
 
 Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Functions'),(Join-Path -Path $PSScriptRoot -ChildPath 'Tasks') -Filter '*.ps1' |
     ForEach-Object { . $_.FullName }
