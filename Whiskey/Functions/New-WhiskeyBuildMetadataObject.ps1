0a8c68c432818beb0f015c0c9a503993cc8f5c91
Aaron Jensen <ajensen@webmd.net>
2017-08-10 16:28:26 -0700

    Define a property before it gets used/referenced. In the reverse order, causes errors when running under Jenkins on PowerShell 4.

--- Whiskey/Functions/New-WhiskeyBuildMetadataObject.ps1
+++ Whiskey/Functions/New-WhiskeyBuildMetadataObject.ps1
@@ -20,8 +20,8 @@ function New-WhiskeyBuildMetadataObject
                             }
     $info |
         Add-Member -MemberType ScriptProperty -Name 'IsAppVeyor' -Value { return $this.BuildServerName -eq 'AppVeyor' } -PassThru |
-        Add-Member -MemberType ScriptProperty -Name 'IsBuildServer' -Value { return -not $this.IsDeveloper } -PassThru |
         Add-Member -MemberType ScriptProperty -Name 'IsDeveloper' -Value { return $this.BuildServerName -eq '' } -PassThru |
+        Add-Member -MemberType ScriptProperty -Name 'IsBuildServer' -Value { return -not $this.IsDeveloper } -PassThru |
         Add-Member -MemberType ScriptProperty -Name 'IsJenkins' -Value { return $this.BuildServerName -eq 'Jenkins' } -PassThru |
         Add-Member -MemberType ScriptProperty -Name 'IsTeamCity' -Value { return $this.BuildServerName -eq 'TeamCity' } -PassThru
 }
