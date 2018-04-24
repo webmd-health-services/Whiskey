db9b5c9378d113cdbeb534fe2251f69f780e4fd1
Aaron Jensen <ajensen@webmd.net>
2018-04-24 10:32:43 -0700

    Test fails on build server.

--- Test/about_Whiskey_Tasks.Tests.ps1
+++ Test/about_Whiskey_Tasks.Tests.ps1
@@ -5,7 +5,7 @@ Set-StrictMode -Version 'Latest'
 & (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-WhiskeyTest.ps1' -Resolve)
 
 Describe 'about_Whiskey_Tasks' {
-    $helpTopic = help 'about_Whiskey_Tasks'
+    $helpTopic = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath '..\Whiskey\en-US\about_Whiskey_Tasks.help.txt' -Resolve) -Raw
 
     $missingTasks = Get-WhiskeyTask |
                         Select-Object -ExpandProperty 'Name' |
