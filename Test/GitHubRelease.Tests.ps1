e688bdc8beb373ab0824be7784681e82ae6dcda8
Aaron Jensen <ajensen@webmd.net>
2018-05-30 18:07:21 -0700

    Creating initial GitHubRelease task.

---
+++ Test/GitHubRelease.Tests.ps1
@@ -0,0 +1,227 @@
+
+#Requires -Version 4
+Set-StrictMode -Version 'Latest'
+
+& (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-WhiskeyTest.ps1' -Resolve)
+
+$apiKeys = @{ }
+
+function GivenApiKey
+{
+    param(
+        $Name,
+        $Value
+    )
+
+    $apiKeys[$Name] = $Value
+}
+
+function GivenAssetUploaded
+{
+    Mock -CommandName 'Invoke-RestMethod' -ModuleName 'Whiskey' -ParameterFilter { $Uri -like '*/assets?*' }
+}
+
+function GivenFile
+{
+    param(
+        $Path,
+        $Content
+    )
+
+    $Content | Set-Content -Path (Join-Path -Path $TestDrive.FullName -ChildPath $Path)
+}
+
+function GivenReleaseDoesNotExist
+{
+    param(
+        $Tag
+    )
+
+    Mock -CommandName 'Invoke-RestMethod' -ModuleName 'Whiskey' -ParameterFilter ([scriptblock]::create("`$Uri -like '*/releases/tags/$Tag'"))
+}
+
+function GivenReleaseCreated
+{
+    param(
+        $ExpectedContent
+    )
+
+    Mock -CommandName 'Invoke-RestMethod' -ModuleName 'Whiskey' `
+         -ParameterFilter ([scriptblock]::Create("`$Uri -like '*/releases' -and `$Method -eq 'POST'")) `
+         -MockWith ([scriptblock]::create("'$ExpectedContent' | ConvertFrom-Json"))
+}
+
+function GivenWhiskeyYml
+{
+    param(
+        $Content
+    )
+
+    $Content | Set-Content -Path (Join-Path -Path $TestDrive.FullName -ChildPath 'whiskey.yml')
+}
+
+function Init
+{
+    $apiKeys = @{ }
+    $commit = $null
+}
+
+function ThenRequest
+{
+    param(
+        [Parameter(Mandatory=$true)]
+        $Should,
+
+        [Parameter(Mandatory=$true)]
+        $ToUri,
+
+        [Microsoft.PowerShell.Commands.WebRequestMethod]
+        $UsedMethod,
+
+        $WithBody,
+
+        $AsContentType,
+
+        $WithFile
+    )
+
+    It ('should {0}' -f $Should) {
+        if( $WithBody )
+        {
+            Assert-MockCalled -CommandName 'Invoke-RestMethod' -ModuleName 'Whiskey' -ParameterFilter { 
+                $DebugPreference = 'Continue'
+                Write-Debug ('Uri   expected  {0}' -f $ToUri)
+                Write-Debug ('      actual    {0}' -f $Uri)
+                Write-Debug ('Body  expected  {0}' -f $WithBody)
+                Write-Debug ('      actual    {0}' -f $Body)
+                $Uri -eq $ToUri -and $Body -eq $WithBody 
+            }
+        }
+
+        if( $UsedMethod )
+        {
+            Assert-MockCalled -CommandName 'Invoke-RestMethod' -ModuleName 'Whiskey' -ParameterFilter { 
+                $DebugPreference = 'Continue'
+                Write-Debug ('Uri     expected  {0}' -f $ToUri)
+                Write-Debug ('        actual    {0}' -f $Uri)
+                Write-Debug ('Method  expected  {0}' -f $UsedMethod)
+                Write-Debug ('        actual    {0}' -f $Method)
+                $Uri -eq $ToUri -and $Method -eq $UsedMethod 
+            }
+        }
+
+        if( $AsContentType )
+        {
+            Assert-MockCalled -CommandName 'Invoke-RestMethod' -ModuleName 'Whiskey' -ParameterFilter { 
+                $DebugPreference = 'Continue'
+                Write-Debug ('Uri          expected  {0}' -f $ToUri)
+                Write-Debug ('             actual    {0}' -f $Uri)
+                Write-Debug ('ContentType  expected  {0}' -f $AsContentType)
+                Write-Debug ('             actual    {0}' -f $ContentType)
+                $Uri -eq $ToUri -and $ContentType -eq $AsContentType 
+            }
+        }
+
+        if( $WithFile )
+        {
+            Assert-MockCalled -CommandName 'Invoke-RestMethod' -ModuleName 'Whiskey' -ParameterFilter { 
+                $DebugPreference = 'Continue'
+                Write-Debug ('Uri     expected  {0}' -f $ToUri)
+                Write-Debug ('        actual    {0}' -f $Uri)
+                $WithFile = Join-Path -Path $TestDrive.FullName -ChildPath $WithFile
+                Write-Debug ('InFile  expected  {0}' -f $WithFile)
+                Write-Debug ('        actual    {0}' -f $InFile)
+                $Uri -eq $ToUri -and $InFile -eq $WithFile 
+            }
+        }
+    }
+}
+
+function ThenSecurityProtocol
+{
+    param(
+        $HasFlag
+    )
+
+    It ('should enable Tls12'){
+        [System.Net.ServicePointManager]::SecurityProtocol.HasFlag($HasFlag) | Should -Be $true
+    }
+}
+
+function WhenRunningTask
+{
+    [CmdletBinding()]
+    param(
+        $OnCommit
+    )
+
+    $Global:Error.Clear()
+
+    $script:failed = $false
+    try
+    {
+        [Whiskey.Context]$context = New-WhiskeyTestContext -ForDeveloper -ConfigurationPath (Join-Path -Path $TestDrive.FullName -ChildPath 'whiskey.yml')
+        foreach( $key in $apiKeys.Keys )
+        {
+            Add-WhiskeyApiKey -Context $context -ID $key -Value $apiKeys[$key]
+        }
+        $context.BuildMetadata.ScmCommitID = $OnCommit
+        $parameter = $context.Configuration['Build'] | Where-Object { $_.ContainsKey('GitHubRelease') } | ForEach-Object { $_['GitHubRelease'] }
+        Invoke-WhiskeyTask -TaskContext $context -Name 'GitHubRelease' -Parameter $parameter
+        $script:context = $context
+
+        It ('should check if release exists') {
+            Assert-MockCalled -CommandName 'Invoke-RestMethod' -ModuleName 'Whiskey' -ParameterFilter { $Uri -like ('*/releases/tags/{0}' -f $parameter['Tag']) }
+        }
+    }
+    catch
+    {
+        Write-Error -ErrorRecord $_
+        $script:failed = $true
+    }
+}
+
+Describe 'GitHubRelease.when the release doesn''t exist' {
+    Init
+    GivenApiKey 'github.com' 'fubarsnafu'
+    GivenWhiskeyYml @'
+Build:
+- GitHubRelease:
+    ApiKeyID: github.com
+    RepositoryName: webmd-health-services/Whiskey
+    Tag: 0.0.0-rc.1
+    ReleaseName: My Release
+    ReleaseNotes: Release Notes
+    Assets:
+    - Path: Whiskey.zip
+      ContentType: fubar/snafu
+      Name: ZIP
+'@
+    GivenFile 'Whiskey.zip' 'WHISKEY_ZIP_FILE'
+    GivenReleaseDoesNotExist '0.0.0-rc.1'
+    GivenReleaseCreated '
+{
+    "upload_url": "https://api.github.com/webmd-health-services/Whiskey/releases/0/assets{?name,label}"
+}
+'
+    GivenAssetUploaded
+    WhenRunningTask -OnCommit 'deadbee'
+    ThenSecurityProtocol -HasFlag ([System.Net.SecurityProtocolType]::Tls12)
+    ThenRequest -Should 'create release' `
+                -ToUri 'https://api.github.com/repos/webmd-health-services/Whiskey/releases' `
+                -UsedMethod Post `
+                -AsContentType 'application/json' `
+                -WithBody @'
+{
+    "target_commitish":  "deadbee",
+    "body":  "Release Notes",
+    "tag_name":  "0.0.0-rc.1",
+    "name":  "My Release"
+}
+'@
+    ThenRequest -Should 'upload asset' `
+                -ToUri 'https://api.github.com/webmd-health-services/Whiskey/releases/0/assets?name=Whiskey.zip&label=ZIP' `
+                -UsedMethod Post `
+                -AsContentType 'fubar/snafu' `
+                -WithFile 'Whiskey.zip'
+}
---
+++ Whiskey/Tasks/New-WhiskeyGitHubRelease.ps1
@@ -0,0 +1,131 @@
+
+function New-WhiskeyGitHubRelease
+{
+    <#
+    .SYNOPSIS
+    
+
+    .DESCRIPTION
+    
+
+    .EXAMPLE
+    
+
+    
+    #>
+    [CmdletBinding()]
+    [Whiskey.Task('GitHubRelease')]
+    param(
+        [Parameter(Mandatory=$true)]
+        [Whiskey.Context]
+        $TaskContext,
+
+        [Parameter(Mandatory=$true)]
+        [hashtable]
+        $TaskParameter
+    )
+
+    Set-StrictMode -Version 'Latest'
+    Use-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+
+    function Invoke-GitHubApi
+    {
+        param(
+            $Endpoint,
+            $Parameter
+        )
+
+
+    }
+
+    $apiKey = Get-WhiskeyApiKey -Context $TaskContext -ID $TaskParameter['ApiKeyID'] -PropertyName 'ApiKeyID'
+    $headers = @{
+	                Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($apiKey + ":x-oauth-basic"))
+                }
+    $baseUri = [uri]'https://api.github.com/repos/{0}' -f $TaskParameter['RepositoryName']
+
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+    function Invoke-GitHubApi
+    {
+        [CmdletBinding(DefaultParameterSetName='NoBody')]
+        param(
+            [Parameter(Mandatory=$true)]
+            [uri]
+            $Uri,
+
+            [Parameter(Mandatory=$true,ParameterSetName='FileUpload')]
+            [string]
+            $ContentType,
+
+            [Parameter(Mandatory=$true,ParameterSetName='FileUpload')]
+            [string]
+            $InFile,
+
+            [Parameter(Mandatory=$true,ParameterSetName='JsonRequest')]
+            $Parameter,
+
+            [Microsoft.PowerShell.Commands.WebRequestMethod]
+            $Method = [Microsoft.PowerShell.Commands.WebRequestMethod]::Post
+        )
+
+        $optionalParams = @{ }
+        if( $PSCmdlet.ParameterSetName -eq 'JsonRequest' )
+        {
+            if( $Parameter )
+            {
+                $optionalParams['Body'] = $Parameter | ConvertTo-Json
+    	        Write-WhiskeyVerbose -Context $TaskContext -Message $optionalParams['Body']
+            }
+            $ContentType = 'application/json'
+        }
+        elseif( $PSCmdlet.ParameterSetName -eq 'FileUpload' )
+        {
+            $optionalParams['InFile'] = $InFile
+        }
+
+	    try
+	    {
+		    Invoke-RestMethod -Uri $Uri -Method $Method -Headers $headers -ContentType $ContentType @optionalParams
+	    }
+	    catch
+	    {
+            Stop-WhiskeyTask -TaskContext $TaskContext -Message ('GitHub API call to "{0}" failed: {1}' -f $uri,$_)
+	    }
+    }
+
+    $tag = $TaskParameter['Tag']
+    $release = Invoke-GitHubApi -Uri ('{0}/releases/tags/{1}' -f $baseUri,[uri]::EscapeUriString($tag)) -Method Get
+
+    $createOrEditMethod = [Microsoft.PowerShell.Commands.WebRequestMethod]::Post
+    if( $release )
+    {
+        $createOrEditMethod = [Microsoft.PowerShell.Commands.WebRequestMethod]::Patch
+    }
+
+	$releaseData = @{
+			            tag_name         = $TaskParameter['Tag']
+			            target_commitish = $TaskContext.BuildMetadata.ScmCommitID
+			            name             = $TaskParameter['ReleaseName']
+			            body             = $TaskParameter['ReleaseNotes']
+		            }
+
+    $release = Invoke-GitHubApi -Uri ('{0}/releases' -f $baseUri) -Parameter $releaseData -Method $createOrEditMethod
+    $release
+
+    $assetIdx = 0
+    foreach( $asset in $TaskParameter['Assets'] )
+    {
+        $assetPath = $asset['Path'] | Resolve-WhiskeyTaskPath -TaskContext $TaskContext -PropertyName ('Assets[{0}].Path' -f $assetIdx++) -PathType File
+        if( -not $assetPath )
+        {
+            continue
+        }
+
+        $assetName = $assetPath | Split-Path -Leaf
+
+        $uri = $release.upload_url -replace '{[^}]+}$'
+        $uri = '{0}?name={1}&label={2}' -f $uri,[uri]::EscapeDataString($assetName),[uri]::EscapeDataString($asset['Name'])
+        Invoke-GitHubApi -Method Post -Uri $uri -ContentType $asset['ContentType'] -InFile $assetPath
+    }
+}
