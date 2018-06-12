c0027494b15e8f305de119a1dcaf3fabc478ce01
Joseph Larionov <jlarionov@webmd.net>
2018-06-12 16:02:41 -0700

    Updated all DotNet tasks TaskContext parameter to take a [Whiskey.Context] object

--- Whiskey/Tasks/Invoke-WhiskeyDotNetPublish.ps1
+++ Whiskey/Tasks/Invoke-WhiskeyDotNetPublish.ps1
@@ -6,7 +6,7 @@ function Invoke-WhiskeyDotNetPublish
     [Whiskey.RequiresTool('DotNet','DotNetPath',VersionParameterName='SdkVersion')]
     param(
         [Parameter(Mandatory=$true)]
-        [object]
+        [Whiskey.Context]
         $TaskContext,
 
         [Parameter(Mandatory=$true)]
--- Whiskey/Tasks/Invoke-WhiskeyDotNetTest.ps1
+++ Whiskey/Tasks/Invoke-WhiskeyDotNetTest.ps1
@@ -6,7 +6,7 @@ function Invoke-WhiskeyDotNetTest
     [Whiskey.RequiresTool('DotNet','DotNetPath',VersionParameterName='SdkVersion')]
     param(
         [Parameter(Mandatory=$true)]
-        [object]
+        [Whiskey.Context]
         $TaskContext,
 
         [Parameter(Mandatory=$true)]
