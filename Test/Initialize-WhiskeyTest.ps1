332b492028b4e425e3b2113f3677f03c5da74fcd
Aaron Jensen <ajensen@webmd.net>
2018-09-13 14:00:53 -0700

    DotNet* tests need to set an environment variable so Carbon is required.

--- Test/Initialize-WhiskeyTest.ps1
+++ Test/Initialize-WhiskeyTest.ps1
@@ -2,6 +2,7 @@
 & (Join-Path -Path $PSScriptRoot -ChildPath '..\Whiskey\Import-Whiskey.ps1' -Resolve)
 & (Join-Path -Path $PSScriptRoot -ChildPath '..\Whiskey\BuildMasterAutomation\Import-BuildMasterAutomation.ps1' -Resolve)
 & (Join-Path -Path $PSScriptRoot -ChildPath '..\Whiskey\ProGetAutomation\Import-ProGetAutomation.ps1' -Resolve)
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\Modules\Carbon' -Resolve) -Force
 
 foreach( $name in @( 'PackageManagement', 'PowerShellGet' ) )
 {
