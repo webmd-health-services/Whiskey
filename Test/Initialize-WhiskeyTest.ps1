36f7f221b7dcbafbade4785beade516b853ef119
Aaron Jensen <ajensen@webmd.net>
2018-12-28 05:18:44 -0800

    Remove ProGetAutomation if it is loaded, otherwise Install-Module fails to re-import if the previous instance was loaded from a Pester test drive (attempt #2).

--- Test/Initialize-WhiskeyTest.ps1
+++ Test/Initialize-WhiskeyTest.ps1
@@ -1,12 +1,13 @@
 
-& (Join-Path -Path $PSScriptRoot -ChildPath '..\Whiskey\Import-Whiskey.ps1' -Resolve)
-
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\PSModules\BuildMasterAutomation' -Resolve) -Force
-
+# Some tests load ProGetAutomation from a Pester test drive. Forcibly remove the module if it is loaded to avoid errors.
 if( (Get-Module -Name 'ProGetAutomation') )
 {
     Remove-Module -Name 'ProGetAutomation' -Force
 }
+
+& (Join-Path -Path $PSScriptRoot -ChildPath '..\Whiskey\Import-Whiskey.ps1' -Resolve)
+
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\PSModules\BuildMasterAutomation' -Resolve) -Force
 Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\PSModules\ProGetAutomation' -Resolve) -Force
 
 foreach( $name in @( 'PackageManagement', 'PowerShellGet' ) )
