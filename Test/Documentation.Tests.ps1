0227927fc9f1a10a47015cdfb008db2b6bc8170d
Aaron Jensen <ajensen@webmd.net>
2019-01-30 01:02:09 -0800

    Trying a better way to not show progress output on build server.

--- Test/Documentation.Tests.ps1
+++ Test/Documentation.Tests.ps1
@@ -4,15 +4,20 @@ Set-StrictMode -Version 'Latest'
 
 & (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-WhiskeyTest.ps1')
 
+Write-Verbose $ProgressPreference -Verbose
+
 Describe ('Documentation') {
     $tasksMissingDocs = Get-WhiskeyTask |
                             Select-Object -ExpandProperty 'Name'|
                             Where-Object { $_ -notin @( 'DotNetBuild', 'DotNetPack', 'DotNetPublish', 'DotNetTest', 'NodeNspCheck', 'Node', 'NpmConfig', 'NpmInstall', 'NpmPrune', 'NpmRunScript', 'PublishNuGetLibrary', 'PublishNuGetPackage' ) } |
                             Where-Object { 
-                                -not (Get-Help -Name ('about_Whiskey_{0}_Task' -f $_))
+                                $ProgressPreference = 'SilentlyContinue'
+                                -not (Get-Help -Name ('about_Whiskey_{0}_Task' -f $_) -Category HelpFile)
                             } 
     It ('should have about_Whiskey_*_Task help topic for every task') {
         $tasksMissingDocs | Should -BeNullOrEmpty
     }
     
 }
+
+Write-Verbose $ProgressPreference -Verbose
