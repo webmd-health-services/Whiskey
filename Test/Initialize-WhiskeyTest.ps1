c60605ec8c1b588bd7f76ac147e9f1d85e1a5a86
Aaron Jensen <ajensen@webmd.net>
2019-06-06 14:38:06 -0700

    Now that verbose messages show in builds, don't show all the module import messages.

--- Test/Initialize-WhiskeyTest.ps1
+++ Test/Initialize-WhiskeyTest.ps1
@@ -1,25 +1,26 @@
 
-# Some tests load ProGetAutomation from a Pester test drive. Forcibly remove the module if it is loaded to avoid errors.
-if( (Get-Module -Name 'ProGetAutomation') )
-{
-    Remove-Module -Name 'ProGetAutomation' -Force
-}
-
-& (Join-Path -Path $PSScriptRoot -ChildPath '..\Whiskey\Import-Whiskey.ps1' -Resolve)
-
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\PSModules\BuildMasterAutomation' -Resolve) -Force
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\PSModules\ProGetAutomation' -Resolve) -Force
-
-foreach( $name in @( 'PackageManagement', 'PowerShellGet' ) )
-{
-    if( (Get-Module -Name $name) )
+& {
+    $VerbosePreference = 'SilentlyContinue'
+    # Some tests load ProGetAutomation from a Pester test drive. Forcibly remove the module if it is loaded to avoid errors.
+    if( (Get-Module -Name 'ProGetAutomation') )
     {
-        Remove-Module -Name $name -Force
+        Remove-Module -Name 'ProGetAutomation' -Force
     }
 
-    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath ('..\Whiskey\Modules\{0}' -f $name) -Resolve) -Force
+    & (Join-Path -Path $PSScriptRoot -ChildPath '..\Whiskey\Import-Whiskey.ps1' -Resolve)
+
+    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\PSModules\BuildMasterAutomation' -Resolve) -Force
+    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\PSModules\ProGetAutomation' -Resolve) -Force
+
+    foreach( $name in @( 'PackageManagement', 'PowerShellGet' ) )
+    {
+        if( (Get-Module -Name $name) )
+        {
+            Remove-Module -Name $name -Force
+        }
+
+        Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath ('..\Whiskey\Modules\{0}' -f $name) -Resolve) -Force
+    }
+
+    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'WhiskeyTest.psm1') -Force
 }
-
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'WhiskeyTest.psm1') -Force
-
-
