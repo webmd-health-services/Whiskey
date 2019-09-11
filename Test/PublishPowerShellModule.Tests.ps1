f2ca87c09a95b2e9ff372b398cfe675c71d56482
Eric Harper <eharper@webmd.net>
2019-09-11 12:58:12 -0700

    Changed a 'PublishPowerShellModule' test to match correct error message in 'Resolve-WhiskeyTaskPathParamater'.

--- Test/PublishPowerShellModule.Tests.ps1
+++ Test/PublishPowerShellModule.Tests.ps1
@@ -555,7 +555,7 @@ Describe 'PublishPowerShellModule.when non-directory path parameter' {
         Initialize-Test
         GivenRepository 'Fubar'
         Invoke-Publish -ForRepositoryNamed 'Fubar' -WithInvalidPath  -ErrorAction SilentlyContinue
-        ThenFailed -WithError 'must\ be\ a\ directory'
+        ThenFailed -WithError 'should\ be\ a\ directory'
         ThenModuleNotPublished
     }
 }
