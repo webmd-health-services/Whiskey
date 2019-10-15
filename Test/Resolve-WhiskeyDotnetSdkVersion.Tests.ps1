555b919fcd4bf0285ac4de5cfb71a163fa0985e1
Aaron Jensen <ajensen@webmd.net>
2019-10-15 14:10:17 -0700

    Fixed: .NET Core's release index is no longer showing all releases.

--- Test/Resolve-WhiskeyDotnetSdkVersion.Tests.ps1
+++ Test/Resolve-WhiskeyDotnetSdkVersion.Tests.ps1
@@ -190,3 +190,13 @@ Describe 'Resolve-WhiskeyDotNetSdkVersion.when latest version API doesn''t retur
         ThenReturnedNothing
     }
 }
+
+Describe 'Resolve-WhiskeyDotNetSdkVersion.when Microsoft moves the index again' {
+    It 'should fail' {
+        Init
+        Mock -CommandName 'Invoke-RestMethod' -ModuleName 'Whiskey'
+        WhenResolvingSdkVersion -Version '2.1' -ErrorAction SilentlyContinue
+        ThenError 'releases index'
+        ThenReturnedNothing
+    }
+}
