4d9258b228eeb3fe20f0eedc7a501610bf5ab4c3
Aaron Jensen <ajensen@webmd.net>
2019-10-28 12:46:44 -0700

    Try not to hard-code values that can be gleaned elsewhere.

--- Whiskey/Tasks/Log.ps1
+++ Whiskey/Tasks/Log.ps1
@@ -15,7 +15,27 @@ function Write-WhiskeyLog
     Set-StrictMode -Version 'Latest'
     Use-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
 
-    $levels = @('Error','Warning','Info','Verbose','Debug')
+    $infoCmd = Get-Command -Name 'Write-WhiskeyInfo' -ModuleName 'Whiskey'
+    if( -not $infoCmd )
+    {
+        Stop-WhiskeyTask -Context $Context -Message ('Umm, we can''t seem to find Whiskey''s Write-WhiskeyInfo function. Something pretty bad has gone wrong.')
+        return
+    }
+
+    $levels = 
+        $infoCmd.Parameters.GetEnumerator() | 
+        Where-Object { $_.Key -eq 'Level' } |
+        Select-Object -ExpandProperty 'Value' |
+        Select-Object -ExpandProperty 'Attributes' |
+        Where-Object { $_ -is [Management.Automation.ValidateSetAttribute] } |
+        Select-Object -ExpandProperty 'ValidValues'
+    
+    if( -not $levels )
+    {
+        Stop-WhiskeyTask -Context $Context -Message ('We can''t seem to find the ValidateSet attribute on the Write-WhiskeyInfo function''s Level parameter. Somethign pretty bad has gone wrong.')
+        return
+    }
+        
     if( $Level -notin $levels )
     {
         Stop-WhiskeyTask -TaskContext $Context -Message ('Property "Level" has an invalid value, "{0}". Valid values are {1}.' -f $Level,($levels -join ", "))
