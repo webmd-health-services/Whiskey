7817481b1f2878e5e72373940708450f1475717a
Aaron Jensen <ajensen@webmd.net>
2019-12-11 11:26:33 -0800

    Pester fixture durations not showing on Linux.

--- Format-PesterResult.ps1
+++ Format-PesterResult.ps1
@@ -8,5 +8,5 @@ Get-Item -Path $Path |
     Get-Content -Raw |
     ForEach-Object { ([xml]$_).SelectNodes('/test-results/test-suite/results/test-suite') } |
     ForEach-Object { [pscustomobject]@{ name = ($_.name | Split-Path -Leaf); duration = ([TimeSpan]::FromSeconds( $_.time )) } } |
-    Sort-Object -Descending -Property 'duration'
-
+    Sort-Object -Descending -Property 'duration' |
+    Format-Table -Auto
