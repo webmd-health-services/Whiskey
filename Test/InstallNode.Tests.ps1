b0821602749394c2282bf80eea5f27ee79210c3f
Joe Leland <jleland@webmd.net>
2020-10-14 12:11:11 -0700

    Allow force install of node and version specification.

--- Test/InstallNode.Tests.ps1
+++ Test/InstallNode.Tests.ps1
@@ -6,27 +6,100 @@ Set-StrictMode -Version 'Latest'
 
 [Whiskey.Context]$context = $null
 
+$script:latestNodeVersion = 
+    Invoke-RestMethod -Uri 'https://nodejs.org/dist/index.json' | 
+    Sort-Object | 
+    Select-Object -ExpandProperty SyncRoot | 
+    Where-Object{ $_.lts -ne $false } | 
+    Select-Object -ExpandProperty version | 
+    Select-Object -First 1
+
 function Init
 {
     $script:testRoot = New-WhiskeyTestRoot
     $script:context = $null
+    $script:fileCreatedTime = New-Object Collections.ArrayList
 }
 
 function ThenNodeInstalled
 {
-    Test-Path -Path (Join-Path -Path $script:testRoot -ChildPath '.node') -PathType Container | Should -BeTrue
+    param(
+        [String]$Version
+    )
+
+    $nodePath = Join-Path -Path $script:testRoot -ChildPath '.node'
+    Test-Path -Path $nodePath -PathType Container | Should -BeTrue
+    if($Version)
+    {
+        Get-ChildItem -Path $nodePath | Where-Object { $_.Name -match '\d+\.\d+\.\d+' } | Select-Object -ExpandProperty Name
+        $Version -match $Matches[0] | Should -BeTrue
+    }
 }
 
 function WhenInstallingNode
 {
+    param(
+        [String]$Version
+    )
+
+    $nodePath = Join-Path -Path $script:testRoot -ChildPath '.node'
     $script:context = New-WhiskeyTestContext -ForDeveloper -ForBuildRoot $script:testRoot
+    $parameters = @{
+        'Version' = $Version
+    }
+    Invoke-WhiskeyTask -TaskContext $context -Parameter $parameters -Name 'InstallNode' 
+    $script:fileCreatedTime.add(((Get-ChildItem -Path $nodePath).CreationTime | Select-Object -ExpandProperty ticks))
+}
+
+function WhenInstallingNodeForce
+{
+    param(
+        [String]$Version
+    )
+    $parameters = @{
+        'Force' = $true
+        'Version' = $Version
+    }
     Invoke-WhiskeyTask -TaskContext $context -Parameter $parameters -Name 'InstallNode'
 }
 
+function ThenNodeFolderDidNotChange
+{
+    @( $filecreatedTime | Select-Object -Unique ).Count -eq 1 | Should -BeTrue
+}
+
 Describe 'InstallNode' {
-    It 'Should install node'{
+    It "Should install latest Node.js $($latestNodeVersion)"{
         Init
         WhenInstallingNode
-        ThenNodeInstalled
+        ThenNodeInstalled -Version $latestNodeVersion
+    }
+}
+
+Describe 'InstallNode Node.js 4.5.0' {
+    It 'Should install Node.js 4.5.0'{
+        Init
+        WhenInstallingNode -Version '4.5.0'
+        ThenNodeInstalled -Version '4.5.0'
+    }
+}
+
+Describe 'InstallNode Node.js 4.5.0, then InstallNode with force' {
+    It "Should overwrite Node.js 4.5.0 with Node.js $($latestNodeVersion)"{
+        Init
+        WhenInstallingNode -Version '4.5.0'
+        ThenNodeInstalled -Version '4.5.0'
+        WhenInstallingNodeForce
+        ThenNodeInstalled -Version $latestNodeVersion
+    }
+}
+
+Describe 'InstallNode node version 4.5.0, then InstallNode 4.5.0 again' {
+    It 'Should not overwrite old node folder'{
+        Init
+        WhenInstallingNode -Version '4.5.0'
+        WhenInstallingNode -Version '4.5.0'
+        ThenNodeInstalled -Version '4.5.0' 
+        ThenNodeFolderDidNotChange
     }
 }
--- Whiskey/Tasks/InstallNode.ps1
+++ Whiskey/Tasks/InstallNode.ps1
@@ -2,7 +2,17 @@ function Install-Node
 {
     [Whiskey.Task('InstallNode')]
     [Whiskey.RequiresTool('Node', PathParameterName='NodePath')]
-    [CmdletBinding()]
     param(
+        [Parameter(Mandatory)]
+        [Whiskey.Context]$TaskContext,
+
+        [Parameter(Mandatory)]
+        [hashtable]$TaskParameter
     )
+
+    if( $TaskParameter['Force'] -or $TaskParameter['Version'] )
+    {
+        #Skips install if specified version is already installed
+        Install-WhiskeyNode -InstallRoot $TaskContext.BuildRoot -Version $TaskParameter['Version']
+    }
 }
