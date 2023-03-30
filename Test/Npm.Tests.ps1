c081e5d63ccd4f6c5b1287320b68df61475a6d15
Aaron Jensen <ajensen@webmd.net>
2023-03-30 11:38:12 -0700

    Migrating test to Pester 5.

--- Test/Npm.Tests.ps1
+++ Test/Npm.Tests.ps1
@@ -2,122 +2,113 @@
 #Requires -Version 5.1
 Set-StrictMode -Version 'Latest'
 
-& (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-WhiskeyTest.ps1' -Resolve)
-
-$failed = $false
-$testRoot = $null
-
-function Init
-{
-    param(
-        [switch]$SkipInstall
-    )
+BeforeAll {
+    & (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-WhiskeyTest.ps1' -Resolve)
 
     $script:failed = $false
-    $script:testRoot = New-WhiskeyTestRoot
-    if( -not $SkipInstall )
+    $script:testRoot = $null
+
+    function GivenNodeInstalled
     {
         Install-Node -BuildRoot $script:testRoot
     }
-}
 
-function ThenFile
-{
-    param(
-        $Named,
-        $Is
-    )
-
-    $path = Join-Path -Path $script:testRoot -ChildPath $Named
-    $path | Should -Exist
-    $path | Should -FileContentMatchMultiline $Is
-}
-
-function ThenTaskFails
-{
-    param(
-        $WithError
-    )
-
-    $failed | Should -BeTrue
-    $Global:Error | Where-Object { $_ -match $WithError } | Should -Not -BeNullOrEmpty
-}
-
-function ThenTaskSucceeds
-{
-    param(
-    )
-
-    $failed | Should -BeFalse
-}
-
-function WhenRunningCommand
-{
-    [CmdletBinding()]
-    param(
-        $Name,
-        $WithArguments
-    )
-
-    $parameters = @{ }
-    if( $Name )
+    function ThenFile
     {
-        $parameters['Command'] = $Name
+        param(
+            $Named,
+            $Is
+        )
+
+        $path = Join-Path -Path $script:testRoot -ChildPath $Named
+        $path | Should -Exist
+        $path | Should -FileContentMatchMultiline $Is
     }
 
-    if( $WithArguments )
+    function ThenTaskFails
     {
-        $parameters['Argument'] = $WithArguments
-    }
-                        
+        param(
+            $WithError
+        )
 
-    $context = New-WhiskeyTestContext -ForBuildServer -ForBuildRoot $script:testRoot
-    $script:failed = $false
-
-    try
-    {
-        $Global:Error.Clear()
-        Invoke-WhiskeyTask -TaskContext $context -Name 'Npm' -Parameter $parameters
+        $script:failed | Should -BeTrue
+        $Global:Error | Where-Object { $_ -match $WithError } | Should -Not -BeNullOrEmpty
     }
-    catch
+
+    function ThenTaskSucceeds
     {
-        $script:failed = $true
-        Write-Error -ErrorRecord $_
+        param(
+        )
+
+        $script:failed | Should -BeFalse
+    }
+
+    function WhenRunningCommand
+    {
+        [CmdletBinding()]
+        param(
+            $Name,
+            $WithArguments
+        )
+
+        $parameters = @{ }
+        if( $Name )
+        {
+            $parameters['Command'] = $Name
+        }
+
+        if( $WithArguments )
+        {
+            $parameters['Argument'] = $WithArguments
+        }
+
+
+        $context = New-WhiskeyTestContext -ForBuildServer -ForBuildRoot $script:testRoot
+        $script:failed = $false
+
+        try
+        {
+            $Global:Error.Clear()
+            Invoke-WhiskeyTask -TaskContext $context -Name 'Npm' -Parameter $parameters
+        }
+        catch
+        {
+            $script:failed = $true
+            Write-Error -ErrorRecord $_
+        }
     }
 }
 
 Describe 'Npm.when command succeeds' {
-    It 'should not fail the build' {
-        Init -SkipInstall
+    BeforeEach {
+        $script:failed = $false
+        $script:testRoot = New-WhiskeyTestRoot
+    }
+
+    It 'should run command' {
         WhenRunningCommand 'config' -WithArguments 'set','fubar','snafu','--userconfig','.npmrc'
         ThenFile '.npmrc' -Is @'
 fubar=snafu
 '@
     }
-}
 
-Describe 'Npm.when command fails' {
     It 'should fail the build' {
-        Init
+        GivenNodeInstalled
         $configPath = (Get-Item -Path $PSScriptRoot).PSDrive.Root
         $configPath = Join-Path -Path $configPath -ChildPath ([IO.Path]::GetRandomFileName())
         $configPath = Join-Path -Path $configPath -ChildPath ([IO.Path]::GetRandomFileName())
         WhenRunningCommand 'k4bphelohjx' -ErrorAction SilentlyContinue
         ThenTaskFails -WithError 'NPM\ command\ "npm\ k4bphelohjx.*"\ failed\ with\ exit\ code\ '
     }
-}
 
-Describe 'Npm.when command not given' {
-    It 'should fail' {
-        Init
+    It 'should require command' {
+        GivenNodeInstalled
         WhenRunningCommand -ErrorAction SilentlyContinue
         ThenTaskFails -WithError 'Property\ "Command\" is required'
     }
-}
 
-Describe 'Npm.when command has no arguments' {
-    It 'should not fail' {
-        Init
+    It 'should allow no arguments' {
+        GivenNodeInstalled
         WhenRunningCommand 'version'
         ThenTaskSucceeds
     }
