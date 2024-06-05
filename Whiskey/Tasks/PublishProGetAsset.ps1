f200815d9e6cee392e0a4a01146d51432ba438a0
Joseph Larionov <jlarionov@webmd.net>
2024-06-05 11:15:48 -0700

    Migrate PublishProGetAsset task implementation to discrete task parameters

--- Whiskey/Tasks/PublishProGetAsset.ps1
+++ Whiskey/Tasks/PublishProGetAsset.ps1
@@ -1,23 +1,28 @@
 
-
 function Publish-WhiskeyProGetAsset
 {
     [Whiskey.Task('PublishProGetAsset')]
     [Whiskey.RequiresPowerShellModule('ProGetAutomation',
                                         Version='2.*',
                                         VersionParameterName='ProGetAutomationVersion')]
+
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingPlainTextForPassword', '')]
     [CmdletBinding()]
     param(
         # The context this task is operating in. Use `New-WhiskeyContext` to create context objects.
         [Whiskey.Context]$TaskContext,
 
-        # The parameters/configuration to use to run the task.
-        [hashtable]$TaskParameter,
+        [String[]] $Path,
+
+        [String[]] $AssetPath,
+
+        [String] $AssetDirectory,
+
+        [String] $CredentialID,
 
         [Alias('Uri')]
         [Uri] $Url
     )
-
 
     Set-StrictMode -Version 'Latest'
     Use-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
@@ -35,34 +40,36 @@ function Publish-WhiskeyProGetAsset
         - ""path/toanother/file.txt""
         AssetDirectory: 'versions'
         "
-    if( -not $TaskParameter['Path'] )
+
+    if (-not $Path)
     {
         Stop-WhiskeyTask -TaskContext $TaskContext -Message ("Please add a valid Path Parameter to your whiskey.yml file:" + $message)
         return
     }
 
-    if( -not $TaskParameter['AssetDirectory'] )
+    if (-not $AssetDirectory)
     {
         Stop-WhiskeyTask -TaskContext $TaskContext -Message ("Please add a valid Directory Parameter to your whiskey.yml file:" + $message)
         return
     }
 
-    if( -Not $TaskParameter['CredentialID'])
+    if (-not $CredentialID)
     {
         Stop-WhiskeyTask -TaskContext $TaskContext -Message ("CredentialID is a mandatory property. It should be the ID of the credential to use when connecting to ProGet. Add the credential with the `Add-WhiskeyCredential` function:" + $message)
         return
     }
 
-    $credential = Get-WhiskeyCredential -Context $TaskContext -ID $TaskParameter['CredentialID'] -PropertyName 'CredentialID'
+    $credential = Get-WhiskeyCredential -Context $TaskContext -ID $CredentialID -PropertyName 'CredentialID'
 
     $session = New-ProGetSession -Uri $Url -Credential $credential -WarningAction Ignore
 
-    $assetDirName = $TaskParameter['AssetDirectory']
+    $assetDirName = $AssetDirectory
     Write-WhiskeyInfo $Url
-    foreach($path in $TaskParameter['Path'])
+
+    foreach($pathItem in $Path)
     {
-        if( $TaskParameter['AssetPath'] -and @($TaskParameter['AssetPath']).count -eq @($TaskParameter['Path']).count){
-            $name = @($TaskParameter['AssetPath'])[$TaskParameter['Path'].indexOf($path)]
+        if ($AssetPath -and @($AssetPath).count -eq ($Path | Measure-Object).Count) {
+            $name = @($AssetPath)[$Path.indexOf($pathItem)]
         }
         else
         {
@@ -70,7 +77,7 @@ function Publish-WhiskeyProGetAsset
             return
         }
 
-        Write-WhiskeyInfo "  $($path | Resolve-WhiskeyRelativePath) -> ${assetDirName}/${name}"
-        Set-ProGetAsset -Session $session -DirectoryName $assetDirName -Path $name -FilePath $path
+        Write-WhiskeyInfo "  $($pathItem | Resolve-WhiskeyRelativePath) -> ${assetDirName}/${name}"
+        Set-ProGetAsset -Session $session -DirectoryName $assetDirName -Path $name -FilePath $pathItem
     }
 }
