06524fc22ee97226bc8cd0f344f967eed027340c
Aaron Jensen <ajensen@webmd.net>
2024-09-24 16:38:35 -0700

    Lint: don't use Attribute part of class name in attributes.

--- Whiskey/Tasks/Delete.ps1
+++ Whiskey/Tasks/Delete.ps1
@@ -1,6 +1,6 @@
 function Remove-WhiskeyItem
 {
-    [Whiskey.TaskAttribute('Delete',SupportsClean)]
+    [Whiskey.Task('Delete',SupportsClean)]
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
@@ -12,7 +12,7 @@ function Remove-WhiskeyItem
 
     Set-StrictMode -Version 'Latest'
     Use-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
-    
+
     foreach( $pathItem in $Path )
     {
         if( -not (Test-Path -Path $pathItem) )
